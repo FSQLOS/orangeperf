49e3537495f8886f50376909cf97fc3129e5277f
badmonkey <fsqlnico@gmail.com>
2026-02-13 18:44:49 +0100

    Mise à jour du 13/02/2026

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()]
+  // REMPLACE 'orangeperf' par le nom exact de ton dépôt GitHub si c'est différent
+  base: '/orangeperf/',
+  plugins: [
+    react(),
+                            VitePWA({
+                              registerType: 'autoUpdate',
+                              manifest: {
+                                name: 'Orange Perf',
+                                short_name: 'OrangePerf',
+                                theme_color: '#FF7900',
+                                background_color: '#f4f6fa',
+                                display: 'standalone',
+                                icons: [
+                                  {
+                                    src: 'pwa-192x192.png',
+                                    sizes: '192x192',
+                                    type: 'image/png'
+                                  }
+                                ]
+                              }
+                            })
+  ]
 })
